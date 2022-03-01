e545b208251708d0cd1be829d9d3e07f99295fd6
Carlos McCrum <69286570+281clo@users.noreply.github.com>
2022-03-01 12:03:46 -0800

    Add chefs

--- app.py
+++ app.py
@@ -2,5 +2,6 @@ from student import Student
 
 student1 = Student("Jim", "Buisness", 3.1, False)
 student2 = Student("Pam", "Art", 2.5, True)
+student3 = Student("Carlos", "Data Science", 4.0, False)
 
-print(student2.gpa)
+print(student3.__dict__)
---
+++ app_chef.py
@@ -0,0 +1,10 @@
+from Chef import Chef
+from Foreign_Chef import ForeignChef
+
+myChef = Chef()
+myChef.make_chicken()
+myChef.make_special_dish()
+
+myForeignChef = ForeignChef()
+myForeignChef.make_chicken()
+myForeignChef.make_special_dish()
